1cb431f4a33e7d86e61fe462a9e105d5b80fd2eb
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2024-02-13 18:08:26 -0300

    feat(home.component): create template elements and style

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiSvgModule } from '@taiga-ui/core';
+import { TuiMarkerIconModule } from '@taiga-ui/kit';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiSvgModule, TuiMarkerIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
