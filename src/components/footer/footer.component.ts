e6e3e1d62bcad2e107bc4dfaf3d4713ed21efaf7
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2024-02-24 14:38:38 -0300

    feat(footert): create method to browse navegate

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -8,4 +8,20 @@ import { TaigaUiModule } from '../../shared/taiga-ui/taiga-ui.module';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  toBrowse(social: string) {
+    const socialUrls: Record<string, string> = {
+      instagram: 'https://www.instagram.com/biomedsandra/',
+      facebook: 'https://www.facebook.com/profile.php?id=100063966036549',
+      linkedin: 'https://www.linkedin.com/in/sandra-kotovicz-591a3b218/',
+    };
+
+    const url = socialUrls[social.toLowerCase()];
+
+    if (url) {
+      window.open(url, '_blank');
+    } else {
+      console.error('Rede social não encontrada:', social);
+    }
+  }
+}
