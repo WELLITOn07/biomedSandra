4f502489fde805b452ef9a6bd1c8dea9b703276a
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2024-03-01 01:40:41 -0300

    fix(index,header,changeTheme.service): adjuste service shared theme

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -2,9 +2,12 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  OnDestroy,
+  OnInit,
 } from '@angular/core';
 import { TaigaUiModule } from '../../shared/taiga-ui/taiga-ui.module';
-import { ChangeThemeService } from '../../services/changeTheme.service'; 'rxjs';
+import { ChangeThemeService } from '../../services/changeTheme.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -14,14 +17,30 @@ import { ChangeThemeService } from '../../services/changeTheme.service'; 'rxjs';
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isDarkTheme: boolean = false;
+  destroySubject = new Subject<void>();
+
   constructor(
     private changeThemeService: ChangeThemeService,
     private cdr: ChangeDetectorRef
   ) {}
 
+  ngOnInit(): void {
+    this.changeThemeService.getTheme()
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe((theme) => {
+        this.isDarkTheme = theme;
+      });
+  }
+
   changeTheme(themeName: string): void {
     this.changeThemeService.changeTheme(themeName);
     this.cdr.detectChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject.next();
+    this.destroySubject.complete();
   }
 }
--- src/services/changeTheme.service.ts
+++ src/services/changeTheme.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChangeThemeService {
-  private readonly themeKey = 'theme';
+  private readonly themeKey = 'user-theme';
+  public isDarkTheme = new BehaviorSubject<boolean>(false);
 
-  changeTheme(themeName: string): void {
+  public changeTheme(themeName: string): void {
     document.body.classList.remove('light-theme', 'dark-theme');
     document.body.classList.add(themeName);
+    const isDarkTheme = Boolean(themeName === 'dark-theme');
+    this.isDarkTheme.next(isDarkTheme);
     localStorage.setItem(this.themeKey, themeName);
+  }
+
+  public getTheme(): Observable<boolean> {
+    const storedTheme = localStorage.getItem(this.themeKey);
+    const isDarkTheme = Boolean(storedTheme === 'dark-theme');
+    this.isDarkTheme.next(isDarkTheme);
+    return this.isDarkTheme.asObservable();
   }
 }
