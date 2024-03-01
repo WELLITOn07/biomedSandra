b5b4ffcf7c0821196b1db7c400ed4ea6cf0d2fc4
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2024-03-01 01:48:35 -0300

    fix(changeTheme.service): adjust isPlatformBrowser

--- src/services/changeTheme.service.ts
+++ src/services/changeTheme.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
@@ -8,18 +9,25 @@ export class ChangeThemeService {
   private readonly themeKey = 'user-theme';
   public isDarkTheme = new BehaviorSubject<boolean>(false);
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   public changeTheme(themeName: string): void {
-    document.body.classList.remove('light-theme', 'dark-theme');
-    document.body.classList.add(themeName);
-    const isDarkTheme = Boolean(themeName === 'dark-theme');
-    this.isDarkTheme.next(isDarkTheme);
-    localStorage.setItem(this.themeKey, themeName);
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.classList.remove('light-theme', 'dark-theme');
+      document.body.classList.add(themeName);
+      const isDarkTheme = Boolean(themeName === 'dark-theme');
+      localStorage.setItem(this.themeKey, themeName);
+
+      this.isDarkTheme.next(isDarkTheme);
+    }
   }
 
   public getTheme(): Observable<boolean> {
-    const storedTheme = localStorage.getItem(this.themeKey);
-    const isDarkTheme = Boolean(storedTheme === 'dark-theme');
-    this.isDarkTheme.next(isDarkTheme);
+    if (isPlatformBrowser(this.platformId)) {
+      const storedTheme = localStorage.getItem(this.themeKey);
+      const isDarkTheme = Boolean(storedTheme === 'dark-theme');
+      this.isDarkTheme.next(isDarkTheme);
+    }
     return this.isDarkTheme.asObservable();
   }
 }
