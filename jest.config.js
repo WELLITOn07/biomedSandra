3c0d5acb009a2fa960dc450198669a8631948395
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2024-04-06 15:36:42 -0300

    feat(jest/ts.config/all/consent/ebook-detail-modal/navbar/home/consent): add jest and removed jasmine and create tests to the components

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+    "<rootDir>/src/spec.ts",
+  ],
+};
