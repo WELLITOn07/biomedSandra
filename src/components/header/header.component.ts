cdfdbb23587541d6c933fa6f428456a634b84a2e
WELLITOn07 <w.elliton2010@hotmail.com>
2024-09-27 15:06:35 -0300

    fix(header): fixed header

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,34 +1,45 @@
-import {
-  Component,
-} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, NavbarComponent, RouterLink],
   template: `
-    <header role="banner">
+    <header role="banner" class="fixed-header">
       <div class="header__container background-theme">
         <div class="header__actions d-flex justify-content-between align-items-center">
           <div class="header__actions header__actions__ebooks d-flex align-items-center">
-
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-              class="feather feather-book-open header__actions__icons header__actions__icons__ebook">
-              <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-              <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
-            </svg>
+            <i class="bi bi-book header__actions__icons header__actions__icons__ebook"></i>
 
             <app-navbar class="header__actions__navbar"></app-navbar>
 
-            <h1 class="header__actions__title m-0">E-books / Cursos - BiomedSandra</h1>
+            <h1 class="header__actions__title m-0">BiomedSandra</h1>
+          </div>
+
+          <!-- Ícone de voltar para home -->
+          <div class="header__back-home" *ngIf="!isHomeRoute">
+            <a routerLink="/home" class="d-flex align-items-center text-light">
+              <i class="bi bi-house-door-fill me-2"></i> Voltar
+            </a>
           </div>
         </div>
         <div class="header__divider"></div>
       </div>
     </header>
   `,
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+
+  constructor(private router: Router) { }
+  isHomeRoute: boolean = false;
+
+  ngOnInit(): void {
+    const currentUrl = this.router.url;
+
+    this.isHomeRoute = currentUrl === '/home';
+  }
+}
